dd298bad1369736b53c04af4139ef9c019c33b4f
SalvIac <salvatore.iacoletti@yahoo.it>
2022-01-18 17:01:58 +0000

    Change name to pyvulnerability

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='pysequentialvulnerability',
+    name='pyvulnerability',
     version='0.0.1',
     description='Tools for SDOF fragility/vulnerability subjected to sequences - Salvatore Iacoletti',
     author='Salvatore Iacoletti',
     author_email='salvatore.iacoletti92@gmail.com',
-    url='https://github.com/SalvIac/pysequentialvulnerability',
+    url='https://github.com/SalvIac/pyvulnerability',
     packages=find_packages(),
     zip_safe=False,
     )
