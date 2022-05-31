ed351b09376135ea5a89c0ece2e583d352e6261f
SalvIac <salvatore.iacoletti@yahoo.it>
2022-05-31 14:38:43 +0100

    Change test pyvulnerability to pysdvuln

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='pyvulnerability',
+    name='pysdvuln',
     version='0.0.1',
     description='Tools for SDOF fragility/vulnerability subjected to sequences - Salvatore Iacoletti',
     author='Salvatore Iacoletti',
     author_email='salvatore.iacoletti92@gmail.com',
-    url='https://github.com/SalvIac/pyvulnerability',
+    url='https://github.com/SalvIac/pysdvuln',
     packages=find_packages(),
     zip_safe=False,
     )
