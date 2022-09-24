82bec775e70f2e1bb59f1869e1b3a21c9d22d30f
SalvIac <salvatore.iacoletti@yahoo.it>
2022-09-24 22:22:46 +0100

    Update vuln_3d.py

--- pysdvuln/vuln_3d.py
+++ pysdvuln/vuln_3d.py
@@ -188,7 +188,7 @@ class Vuln3d():
         return df
     
 
-    def plot_vuln_surf(self, unit="g", save=False, path="", max_img=None):
+    def check_plots(self, unit="g", save=False, path="", max_img=None):
         X, Y = np.meshgrid(self.X, self.Y)
         if max_img is None:
             max_img = np.max(X)+1e-2
@@ -286,4 +286,58 @@ class Vuln3d():
                         bbox_inches='tight', dpi=600, format="png")
         else:
             plt.show()
+
+
+
+    def plot_vuln_surf(self, unit="g", max_img=None):
+        X, Y = np.meshgrid(self.X, self.Y)
+        if max_img is None:
+            max_img = np.max(X)+1e-2
+        cond = np.logical_and(X <= max_img, Y <= max_img)
+        inds = np.where(cond)
+        n = int(np.sqrt(np.sum(cond)))
+
+        # 3d vulnerability surface vs GM1 and GM2
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        ims = self.X
+
+        if self.ddvc is not None:
+            vuln = self.ddvc.get_vuln_curve_ds1(ds1=0)
+            ax.plot(ims[ims<=max_img],
+                    [0.]*len(ims[ims<=max_img]),
+                    vuln[ims<=max_img], color="k", lw=2, zorder=200)
+            ax.plot([0.]*len(ims[ims<=max_img]),
+                    ims[ims<=max_img],
+                    vuln[ims<=max_img], color="k", lw=2, zorder=200)
+            for ds1 in range(1,len(self.ddvc.vulns.keys())):
+                vuln_ds = self.ddvc.get_vuln_curve_ds1(ds1=ds1)
+                ind = np.searchsorted(vuln, vuln_ds[0])
+                _ims = ims[ind-1:ind+1]
+                _vuln = vuln[ind-1:ind+1]
+                _ims_inter = _ims[0] + np.diff(_ims)/np.diff(_vuln) * (vuln_ds[0]-_vuln[0])
+                ax.plot(_ims_inter*np.ones(len(ims[ims<=max_img])),
+                        ims[ims<=max_img], vuln_ds[ims<=max_img],
+                        color="k", lw=2, zorder=200)
+        surf = ax.plot_surface(X[inds[0], inds[1]].reshape(n,n),
+                               Y[inds[0], inds[1]].reshape(n,n),
+                               self.Z[inds[0], inds[1]].reshape(n,n),
+                               cmap=cm.coolwarm, linewidth=0,
+                               antialiased=False, alpha=0.5)
+        ax.contour(X[inds[0], inds[1]].reshape(n,n),
+                   Y[inds[0], inds[1]].reshape(n,n),
+                   self.Z[inds[0], inds[1]].reshape(n,n),
+                   zdir='z', offset=0.01,
+                   cmap=cm.coolwarm)
+        ax.set_xlabel('{} G1 ({})'.format(self.imt, unit))
+        ax.set_ylabel('{} G2 ({})'.format(self.imt, unit))
+        ax.set_zlabel("Mean Loss Ratio")#("$E(LR | IM_{G1}, IM_{G2})$") #'Loss Ratio')
+        ax.set_xlim([0.,max_img])
+        ax.set_ylim([0.,max_img])
+        ax.set_zlim([0.,1.])
+        ax.view_init(elev=25, azim=-140)
+        plt.show()
+        return ax
+
+
+
 
