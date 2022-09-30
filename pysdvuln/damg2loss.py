bc13c58e25a62a77c1207056a5fe6efb2533395d
Iacoletti <salvatore.iacoletti@axaxl.com>
2022-09-30 16:08:44 +0100

    Better handling of the cov in Damg2Loss

--- pysdvuln/damg2loss.py
+++ pysdvuln/damg2loss.py
@@ -27,15 +27,15 @@ class Damg2Loss():
     
     damg = np.linspace(0,1,101)
     
-    def __init__(self, mean_loss, cov_loss=None):
-        self.mean_loss = mean_loss
-        if cov_loss is None:
-            cov_loss = [None]*len(mean_loss)
-        self.cov_loss = cov_loss
-        if self.mean_loss[0] != 0.:
+    def __init__(self, mean_lr, cov_lr=None):
+        self.mean_lr = np.array(mean_lr)
+        if cov_lr is None:
+            cov_lr = [0.]*len(mean_lr)
+        self.cov_lr = np.array(cov_lr)
+        if self.mean_lr[0] != 0.:
             warnings.warn("Adding 0 mean loss to represent DS0")
-            self.mean_loss.insert(0, 0.)
-            self.cov_loss.insert(0, None)
+            self.mean_lr = np.insert(self.mean_lr, 0, 0.)
+            self.cov_lr = np.insert(self.cov_lr, 0, 0.)
         
         
     @classmethod
@@ -43,7 +43,7 @@ class Damg2Loss():
         '''
         from Martins and Silva (2020): DS0, DS1, DS2, DS3, DS4
         '''
-        return cls([0., 0.05, 0.2, 0.6, 1.], [None, 0.3, 0.2, 0.1, 0.])
+        return cls([0., 0.05, 0.2, 0.6, 1.], [0., 0.3, 0.2, 0.1, 0.])
 
 
     @classmethod
@@ -55,22 +55,38 @@ class Damg2Loss():
         return beta(a, b)
 
 
-    def get_mean_loss(self):
-        mean_loss = self.mean_loss
+    def get_mean_lr(self):
+        mean_lr = self.mean_lr
         # check with integrals
-        # mean_loss = list()
-        # for mu, cov in zip(self.mean_loss, self.cov_loss):
+        # mean_lr = list()
+        # for mu, cov in zip(self.mean_lr, self.cov_lr):
         #     lr, pr_lr = self.get_pmf(self.damg, mu, cov)
-        #     mean_loss.append( np.sum(lr * pr_lr) )
+        #     mean_lr.append( np.sum(lr * pr_lr) )
         # using scipy.integrate
-        # for mu, cov in zip(self.mean_loss, self.cov_loss):
+        # for mu, cov in zip(self.mean_lr, self.cov_lr):
         #     if mu == 0. or mu == 1. or cov == 0. or cov is None:
-        #         mean_loss.append( mu )
+        #         mean_lr.append( mu )
         #     else:
         #         distr = self.get_distr(mu, cov)
-        #         mean_loss.append( integrate.quad(lambda x: x*distr.pdf(x), 0, 1.)[0] )
-        return mean_loss
+        #         mean_lr.append( integrate.quad(lambda x: x*distr.pdf(x), 0, 1.)[0] )
+        return mean_lr
 
+
+    def get_cov_lr(self):
+        return self.cov_lr
+
+
+    def get_var_lr(self):
+        var = list()
+        for mu, cov in zip(self.mean_lr, self.cov_lr):
+            if cov == 0. or cov is None:
+                var.append(0.)
+            else:
+                distr = self.get_distr(mu, cov)
+                _, v, _, _ = distr.stats(moments='mvsk')
+                var.append(v)
+        return np.array(var)
+    
 
     @classmethod
     def get_pmf(cls, damg, mu, cov):
@@ -82,7 +98,7 @@ class Damg2Loss():
 
     def plot(self):
         fig, ax = plt.subplots()
-        for ds, (mu, cov) in enumerate(zip(self.mean_loss, self.cov_loss)):
+        for ds, (mu, cov) in enumerate(zip(self.mean_lr, self.cov_lr)):
             if mu != 0. and mu != 1.:
                 distr = self.get_distr(mu, cov)
                 ax.plot(self.damg, distr.pdf(self.damg)*0.01,
@@ -97,7 +113,7 @@ class Damg2Loss():
         
         
     def get_num_ds(self):
-        return len(self.mean_loss)-1 # first is DS0 (we don't count it)
+        return len(self.mean_lr)-1 # first is DS0 (we don't count it)
         
         
     def __repr__(self):
@@ -105,9 +121,6 @@ class Damg2Loss():
         
     def __str__(self):
         return "<{}, mean: {}, cov: {}>".format(self.__class__.__name__,
-                                                self.mean_loss,
-                                                self.cov_loss)
-
-
-
+                                                list(self.mean_lr),
+                                                list(self.cov_lr))
 
